038046b18e190803ecebe0cc9dbaa2380245b660
kimayeong <kimayeong@valofe.com>
2023-08-16 18:33:23 +0900

    #7-3 React Router

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,38 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate(); // Page Moving
+
+  const [searchParams, setSerchParams] = useSearchParams(); // Query String
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>this is Edit page.</p>
+      <button onClick={() => setSerchParams({ who: "winterwood" })}>
+        QS change
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back
+      </button>
     </div>
   );
 };
