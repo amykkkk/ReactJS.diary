6ad796491a5b05a9aef71d31dfaf2dbf125b82ed
kimayeong <kimayeong@valofe.com>
2023-08-17 11:21:22 +0900

    #7-4 프로젝트 기초공사 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouterTest from "./components/RouterTest";
-
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
+  /*
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  */
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"Left Button"}
+              onClick={() => alert("Left Click")}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"Right Button"}
+              onClick={() => alert("Right Click")}
+            />
+          }
+        />
         <h2>App.js</h2>
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} /> */}
+
+        <MyButton
+          text={"Button"}
+          onClick={() => alert("Button Click")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"Button"}
+          onClick={() => alert("Button Click")}
+          type={"negative"}
+        />
+        <MyButton text={"Button"} onClick={() => alert("Button Click")} />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouterTest />
       </div>
     </BrowserRouter>
   );
